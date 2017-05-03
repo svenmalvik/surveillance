81b29188ef1e0c64ebd0b68ff99a60ffffeeb95d
Sven Malvik <sven@malvik.de>
2017-05-03 20:05:37 +0200

    Changed recorder

--- sstreamaudio.sh
+++ sstreamaudio.sh
@@ -4,8 +4,9 @@ date=`date +%Y-%m-%d:%H:%M:%S`
 dir="/var/www/html"
 file="$dir/stue_$date"
 
-padsp streamer -t 0:60 -O $file.wav -F mono8
-lame $file.wav $file.mp3
+arecord -f cd -d 60 -t wav | lame - $file.mp3
+#padsp streamer -t 0:60 -O $file.wav -F mono8
+#lame $file.wav $file.mp3
 
-rm $file.wav
+#rm $file.wav
 
